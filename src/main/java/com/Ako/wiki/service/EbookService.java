c6d711e8abed8ceacd7d1060dcbb9cff7a9419c2
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-22 11:51:11 -0400

    3-8 Create CopyUtil, which encapsulates BeanUtils to copy variables from Ebook to EbookResp.

--- src/main/java/com/Ako/wiki/service/EbookService.java
+++ src/main/java/com/Ako/wiki/service/EbookService.java
@@ -5,12 +5,15 @@ import com.Ako.wiki.domain.EbookExample;
 import com.Ako.wiki.mapper.EbookMapper;
 import com.Ako.wiki.req.EbookReq;
 import com.Ako.wiki.resp.EbookResp;
+import com.Ako.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.Ako.wiki.util.CopyUtil.copyList;
 
 @Service
 public class EbookService {
@@ -23,16 +26,19 @@ public class EbookService {
         criteria.andNameLike("%"+ req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            // ebookResp.setId(ebook.getId()); // You can use it, but it is redundant.
-            // It is better to use API provided by Spring.
-            // You do not manually set id, name, and a bunch of things.
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            //  EbookResp ebookResp = new EbookResp();
+//            // ebookResp.setId(ebook.getId()); // You can use it, but it is redundant.
+//            // It is better to use API provided by Spring.
+//            // You do not manually set id, name, and a bunch of things.
+//            //  BeanUtils.copyProperties(ebook, ebookResp);
+//            -- Copy the Object
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        // -- Copy all list.
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
