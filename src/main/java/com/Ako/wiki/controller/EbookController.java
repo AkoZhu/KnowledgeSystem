27f8ba8708f656d5edba61ac777d5f353a8747c0
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-22 10:39:26 -0400

    3-6 Add general return class called CommonResp. All return value from backend should be CommonResp class.

--- src/main/java/com/Ako/wiki/controller/EbookController.java
+++ src/main/java/com/Ako/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.Ako.wiki.controller;
 
 import com.Ako.wiki.domain.Ebook;
+import com.Ako.wiki.resp.CommonResp;
 import com.Ako.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list") // we can change "/ebook/list" to "/list" since we add @RequestMapping("/ebook");
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
