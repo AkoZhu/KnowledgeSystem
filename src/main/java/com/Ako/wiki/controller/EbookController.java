ddef5d8de88c74e6f905d07ebe91fef6d8274ac2
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-22 11:22:24 -0400

    3-7 Encapsulate request parameters and return parameters.

--- src/main/java/com/Ako/wiki/controller/EbookController.java
+++ src/main/java/com/Ako/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.Ako.wiki.controller;
 
-import com.Ako.wiki.domain.Ebook;
+//import com.Ako.wiki.domain.Ebook;
+import com.Ako.wiki.req.EbookReq;
 import com.Ako.wiki.resp.CommonResp;
+import com.Ako.wiki.resp.EbookResp;
 import com.Ako.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list") // we can change "/ebook/list" to "/list" since we add @RequestMapping("/ebook");
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req) {
+        // Don't let the whole information, the Ebook class show in Controller.
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
