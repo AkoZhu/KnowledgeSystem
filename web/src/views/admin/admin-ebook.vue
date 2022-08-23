39c448dabdd132502c00c83f259ab59e32fa1859
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-23 17:37:54 -0400

    6-3 Modify Ebook management page.

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>Ebook management page</h1>
-  </div>
+    <a-layout>
+      <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        <!-- Content -->
+        <div class="about">
+          <h1>Ebook management page</h1>
+        </div>
+      </a-layout-content>
+    </a-layout>
 </template>
