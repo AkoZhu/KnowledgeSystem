552a61cfc39fdae504b62c562ad2d19d6ea9d092
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-23 15:56:58 -0400

    5-9 Add AOP, print Interface runtime, query input argument and return argument. Remove filter and Interceptor.

--- src/main/java/com/Ako/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/Ako/wiki/config/SpringMvcConfig.java
@@ -1,19 +1,19 @@
-package com.Ako.wiki.config;
+// package com.Ako.wiki.config;
 
-import com.Ako.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import com.Ako.wiki.interceptor.LogInterceptor;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.annotation.Resource;
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
+// import javax.annotation.Resource;
+// @Configuration
+// public class SpringMvcConfig implements WebMvcConfigurer {
 
-    @Resource
-    LogInterceptor logInterceptor;
+//     @Resource
+//     LogInterceptor logInterceptor;
 
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//     public void addInterceptors(InterceptorRegistry registry) {
+//         registry.addInterceptor(logInterceptor)
+//                 .addPathPatterns("/**").excludePathPatterns("/login");
+//     }
+// }
