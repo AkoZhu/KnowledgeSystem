07681356a9893b9794ae98c702fe9ae696d1abae
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-23 23:49:26 -0400

    6-7 Development Ebook form in Modal window. Show corresponding data in Ebook form.

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -17,7 +17,7 @@
           </template>
           <template v-slot:action="{ text, record }">
             <a-space size="small">
-              <a-button type="primary" @click="edit">
+              <a-button type="primary" @click="edit(record)">
                 Edit
               </a-button>
               <a-button type="danger">
@@ -35,7 +35,23 @@
       :confirm-loading="modalLoading"
       @ok="handleModelOk"
     >
-      <p>test</p>
+      <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+        <a-form-item label="Cover">
+          <a-input v-model:value= "ebook.cover" />
+        </a-form-item>
+        <a-form-item label="Name">
+          <a-input v-model:value="ebook.name" />
+        </a-form-item>
+        <a-form-item label="Category1">
+          <a-input v-model:value="ebook.category1Id" />
+        </a-form-item>
+        <a-form-item label="Category2">
+          <a-input v-model:value="ebook.category2Id" />
+        </a-form-item>
+        <a-form-item label="Description">
+          <a-input v-model:value="ebook.desc" type="textarea" />
+        </a-form-item>
+      </a-form>
     </a-modal>
 </template>
 
@@ -125,6 +141,7 @@
       };
 
       // ----- Ebooks Form ------
+      const ebook = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const handleModelOk = () => {
@@ -139,8 +156,9 @@
        *  Edit 
       */
      
-     const edit = () =>{
+     const edit = (record: any) =>{
       modalVisible.value = true;
+      ebook.value = record
      };
 
 
@@ -159,6 +177,7 @@
         handleTableChange,
 
         edit,
+        ebook,
         modalVisible,
         modalLoading,
         handleModelOk
