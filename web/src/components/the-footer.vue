cd0a5e67c579d82f6f8a48413925c5f00d611c6b
Xuanbiao Zhu <zxuanbiao@outlook.com>
2022-08-27 20:27:52 -0400

    9-9 Use vuex to store user info, so it is convenient to access login info. Rest problem: Value missing after you refresh.

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,20 @@
 <template>
     <a-layout-footer style="text-align: center">
-      Wiki Ebooks
+      Wiki Ebooks! Welcome : {{user.name}};
     </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import store from '@/store';
+import { defineComponent, computed} from 'vue';
 
 export default defineComponent({
   name: 'the-footer',
+  setup() {
+    const user = computed(() => {return store.state.user} );
+    return {
+      user
+    }
+  }
 });
 </script>
